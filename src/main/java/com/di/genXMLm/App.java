c7bb071eb6649726935576a5903012cc3ae2ee96
DIndyk <dialle989@gmail.com>
2015-08-04 11:56:27 +0200

    v2

--- src/main/java/com/di/genXMLm/App.java
+++ src/main/java/com/di/genXMLm/App.java
@@ -13,21 +13,24 @@ import java.io.FileOutputStream;
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Random;
+import java.util.Scanner;
  
 public class App
  
 {
     public static void main(String[] args) throws JAXBException, DatatypeConfigurationException, FileNotFoundException
     {
- 
+    	System.out.println("Wprowadz liczbe autorow:10");
+    	Scanner read = new Scanner(System.in); //obiekt do odebrania danych od użytkownika
+    	int autorow = read.nextInt();
     	HashMap<String, Integer> counts= new HashMap<String, Integer>();
     	
     	//SETUP//
-    	double mnoznik = 10;
-    	counts.put("authors", (int)(10*mnoznik));
-    	counts.put("customers", (int)(100*mnoznik));
-    	counts.put("books", (int)(1000*mnoznik));
-    	counts.put("order", (int)(10000*mnoznik));
+    	double mnoznik = 1;   // mnoznik 1 = 0,6 MB danych
+    	counts.put("authors", autorow);
+    	counts.put("customers", 5*autorow);
+    	counts.put("books", 10*autorow);
+    	counts.put("order", 25*autorow);
     	//SETUP - end//
     	
         ObjectFactory factory = new ObjectFactory();
@@ -36,10 +39,26 @@ public class App
         for(int i=0; i<counts.get("authors"); i++)
         {
         	AuthorType author = factory.createAuthorType();
-        	author.setFirstName(randomString(true, 10, true));
-        	author.setLastName(randomString(true, 10, true));
-        	author.setPseudonym(randomString(true, 10, false));
-        	author.setCode(randomString(false, 15, false));
+        	author.setFirstName(randomString(true, 10, true, true));
+        	author.setLastName(randomString(true, 10, true, true));
+        	author.setPseudonym(randomString(true, 10, false, true));
+        	
+        	String code = "";
+        	int result;
+        	do
+        	{
+        		result = 0;
+        		code = randomString(false, 15, false, false);
+        		if(authors.getAuthor().size() > 0)
+        		{
+	         		for(int test = 0; test < authors.getAuthor().size(); test++ )
+	        		{
+	        			if(code.contentEquals(authors.getAuthor().get(test).getCode()))result = 1;
+	        		}
+        		}
+        	}while(result != 0);
+        		
+        	author.setCode(code);
         	authors.getAuthor().add(author);
         }
         System.out.println("Generacja autorow zakonczona");
@@ -48,13 +67,71 @@ public class App
         for(int i=0; i<counts.get("customers"); i++)
 		{
 			CustomerType customer = factory.createCustomerType();
-			customer.setCode(randomString(false, 15, false));
-			customer.setBarCode(randomString(false, 32, false));
-			customer.setFirstName(randomString(true, 10, true));
-			customer.setLastName(randomString(true, 10, true));
-			customer.setPesel(randomString(false, 10, false));
-			customer.setPhone(randomString(false, 9, false));
-			customer.setAddress(randomString(true, 32, true));
+			
+        	// Unique code
+        	String code = "";
+        	int result;
+        	do
+        	{
+        		result = 0;
+        		code = randomString(false, 15, false, false);
+        		if(customers.getCustomer().size() > 0)
+        		{
+	         		for(int test = 0; test < customers.getCustomer().size(); test++ )
+	        		{
+	        			if(code.contentEquals(customers.getCustomer().get(test).getCode()))result = 1;
+	        		}
+        		}
+        	}while(result != 0);
+        	customer.setCode(code);
+
+        	// Unique Barcode
+        	do
+        	{
+        		result = 0;
+        		code = randomString(false, 32, false, false);
+        		if(customers.getCustomer().size() > 0)
+        		{
+	         		for(int test = 0; test < customers.getCustomer().size(); test++ )
+	        		{
+	        			if(code.contentEquals(customers.getCustomer().get(test).getBarCode()))result = 1;
+	        		}
+        		}
+        	}while(result != 0);      	
+			customer.setBarCode(code);
+			customer.setFirstName(randomString(true, 10, true, true));
+			customer.setLastName(randomString(true, 10, true, true));
+			
+        	// Unique Pesel
+        	do
+        	{
+        		result = 0;
+        		code = randomString(false, 10, false, false);
+        		if(customers.getCustomer().size() > 0)
+        		{
+	         		for(int test = 0; test < customers.getCustomer().size(); test++ )
+	        		{
+	        			if(code.contentEquals(customers.getCustomer().get(test).getPesel()))result = 1;
+	        		}
+        		}
+        	}while(result != 0);
+			customer.setPesel(code);
+			
+        	// Unique Phone
+        	do
+        	{
+        		result = 0;
+        		code = randomString(false, 9, false, false);
+        		if(customers.getCustomer().size() > 0)
+        		{
+	         		for(int test = 0; test < customers.getCustomer().size(); test++ )
+	        		{
+	        			if(code.contentEquals(customers.getCustomer().get(test).getPhone()))result = 1;
+	        		}
+        		}
+        	}while(result != 0);
+			customer.setPhone(code);
+			customer.setAddress(randomString(true, 32, true, true));
 			customers.getCustomer().add(customer);
 		}
         System.out.println("Generacja klientow zakonczona");
@@ -65,40 +142,76 @@ public class App
         	BookType book = factory.createBookType();
         	int authorID = (int)Math.round(Math.random()*(counts.get("authors")-1));
         	book.setAuthorCode(authors.getAuthor().get(authorID).getCode());
-        	book.setCode(randomString(false, 15, false));
-        	book.setDescription(randomString(true, 255, true));
-        	book.setISBN(randomString(false, 12, false));
-        	int price = (int) Math.round(Math.random()*200);
+        	
+        	// Unique BookCode
+        	String code = "";
+        	int result;
+        	do
+        	{
+        		result = 0;
+        		code = randomString(false, 15, false, true);
+        		if(books.getBook().size() > 0)
+        		{
+	         		for(int test = 0; test < books.getBook().size(); test++ )
+	        		{
+	        			if(code.contentEquals(books.getBook().get(test).getCode()))test = 1;
+	        		}
+        		}
+        	}while(result != 0);
+        	book.setCode(code);
+        	
+        	book.setDescription(randomString(true, 255, true, true));
+        	
+        	// Unique ISBN
+        	String isbn = "";
+        	do
+        	{
+        		result = 0;
+        		isbn = randomString(false, 12, false, false);
+        		if(books.getBook().size() > 0)
+        		{
+	         		for(int test = 0; test < books.getBook().size(); test++ )
+	        		{
+	        			if(isbn.contentEquals(books.getBook().get(test).getISBN()))result = 1;
+	        		}
+        		}
+        	}while(result != 0);
+        	book.setISBN(isbn);
+        	
+        	
+        	int price = 10+(int) Math.round(Math.random()*190);
         	book.setPrice(Integer.toString(price));
-        	book.setTitle(randomString(true, 32, true));
+        	book.setTitle(randomString(true, 32, true, true));
         	books.getBook().add(book); 	
         }
         System.out.println("Generacja ksiazek zakonczona");
         
         OrdersType orders = factory.createOrdersType();
-        for(int i = 0; i<counts.get("books"); i++)
+        for(int i = 0; i<counts.get("customers"); i++)
         {
-        	OrderType order = factory.createOrderType();
-        	order.setBookCode("ex");
-        	
-        	//randomowa data
-        	XMLGregorianCalendar cal = DatatypeFactory.newInstance().newXMLGregorianCalendar();
-        	cal.setYear(1950+(int)Math.round(Math.random()*64));
-        	cal.setMonth((int)Math.round(Math.random()*11)+1);
-        	cal.setDay((int)Math.round(Math.random()*30)+1);
-        	cal.setHour((int)Math.round(Math.random()*23));
-        	cal.setMinute((int)Math.round(Math.random()*59));
-        	cal.setSecond((int)Math.round(Math.random()*59));
-        	order.setOrderDate(cal);
-        	
-        	//random customerCode
-        	int customerID = (int)Math.round(Math.random()*(counts.get("customers")-1));
-        	order.setCustomerCode(customers.getCustomer().get(customerID).getCode());
-        	
-        	int bookID = (int)Math.round(Math.random()*(counts.get("books")-1));
-        	order.setBookCode(books.getBook().get(bookID).getCode());
-        	
-        	orders.getOrder().add(order);
+        	for(int cust_ord = 0; cust_ord<5; cust_ord++)
+        	{
+	        	OrderType order = factory.createOrderType();
+	        	order.setBookCode("ex");
+	        	
+	        	//randomowa data
+	        	XMLGregorianCalendar cal = DatatypeFactory.newInstance().newXMLGregorianCalendar();
+	        	cal.setYear(1950+(int)Math.round(Math.random()*64));
+	        	cal.setMonth((int)Math.round(Math.random()*11)+1);
+	        	cal.setDay((int)Math.round(Math.random()*30)+1);
+	        	cal.setHour((int)Math.round(Math.random()*23));
+	        	cal.setMinute((int)Math.round(Math.random()*59));
+	        	cal.setSecond((int)Math.round(Math.random()*59));
+	        	order.setOrderDate(cal);
+	        	
+	        	//random customerCode
+	        	order.setCustomerCode(customers.getCustomer().get(i).getCode());
+	        	
+	        	int bookID = (int)Math.round(Math.random()*(counts.get("books")-1));
+	        	order.setBookCode(books.getBook().get(bookID).getCode());
+	        	
+	        	orders.getOrder().add(order);
+        	}
         }
         System.out.println("Generacja zamowien zakonczona");
         
@@ -120,12 +233,12 @@ public class App
     
 
 
-		static String randomString(boolean znaki, int length, boolean firstBig) {
+		static String randomString(boolean znaki, int length, boolean firstBig, boolean dynamicLength) {
 			String chars = "";
-			if(znaki)chars = "abcdefghijklmnopqrstuvwxyz"; else chars = "0123456789";
+			if(znaki)chars = "abcdefghijklmnopqrstuvwxyz "; else chars = "0123456789";
 			
 		  Random rand = new Random();
-		  length = (int) (5+Math.round((Math.random()*(length-5))));
+		 if(dynamicLength) length = (int) (5+Math.round((Math.random()*(length-5))));
 		  StringBuilder buf = new StringBuilder();
 		  for (int i=0; i<length; i++) {
 		    buf.append(chars.charAt(rand.nextInt(chars.length())));
